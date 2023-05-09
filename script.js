3b5b6599487cdc7cc697a95dd15ff1271c81db9b
NabeelShahzad-1928 <nabeelcohan34@gmail.com>
2023-05-09 12:34:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// var color = ['red','green','orang','blue'];
+// var btn =document.getElementById('btn');
+// function newColor(){
+//     var randomColor=color[Math.floor(Math.random()*color.length)];
+//     document.body.style.background=randomColor;
+// }
+
+var student = [
+    {
+        name: "nabeel",
+        age: 12
+        email: "nabeel123"
+    },
+    {
+        name: "umair",
+        age: 14,
+        email: "meri123"
+    },
+    {
+        name: "ahmad",
+        age: 14,
+        email: "ahmed123"
+    }
+]
+    document.write(student[0].name +" "+ student[1].age  + "<br>");
